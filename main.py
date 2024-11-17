d30518317bde7900cc3ffce2b227dd1392c873bd
Yaromir Sheydaev <148737776+tyriksheyh4567@users.noreply.github.com>
2024-11-17 16:42:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,39 +16,26 @@ def tasks_suggestion(bot, chat_id, user_input):
         "Сделайте массаж стоп с помощью теннисного мяча",
         "Сделайте массаж стоп с помощью массажного ролика"
     ]
-    @bot.message_handler(func=lambda message: True)
-    def kk(message):
-        chat_id = message.chat.id
-        bot.send_message(chat_id, "Неверное количество упражнений. Пожалуйста, введите число от 1 до 10.")
+
     if user_input.isdigit():
         num_exercises = int(user_input)
-        if num_exercises > 0 and num_exercises <= len(exercises):
+        if 0 < num_exercises <= len(exercises):
             suggested_exercises = random.sample(exercises, num_exercises)
-            for exercise in suggested_exercises:
-                bot.send_message(chat_id, exercise)
-            bot.send_message(chat_id, "Пожалуйста, введите количество упражнений.")
+            bot.send_message(chat_id, "\n".join(suggested_exercises))
         else:
-            kk(chat_id)
+            bot.send_message(chat_id, "Неверное количество упражнений. Пожалуйста, введите число от 1 до 10.")
     else:
         bot.send_message(chat_id, "Пожалуйста, введите количество упражнений.")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    """sumary_line"""
-    bot.reply_to(message, "Привет! Я могу предложить вам упражнения для стоп.")
+    """Sends welcome"""
+    bot.send_message(message.chat.id, "Привет! Я могу предложить вам упражнения для стоп.")
 
-def check_number(func):
-    def wrapper(message):
-        if message.text.isdigit():
-            return func(message)
-        else:
-            bot.send_message(message.chat.id, "Пожалуйста, введите количество упражнений.")
-    return wrapper
+check_number = lambda func: lambda message: func(message) if message.text.isdigit() else bot.send_message(message.chat.id, "Пожалуйста, введите количество упражнений.")
 
 @bot.message_handler(func=check_number)
-def echo_all(message):
-    #Lol, it doesn't do echo. It calls a function that suggest exercises.
-    #Btw, I'm too lazy to rename this function.
+def suggest_exercises(message):
     chat_id = message.chat.id
     bot.reply_to(message, tasks_suggestion(bot=bot, chat_id=chat_id, user_input=message.text))
     bot.reply_to(message, "Пожалуйста, введите количество упражнений.")
